88983541ca06316f661a0d038e7b2516ac16899b
Yasutaka ATARASHI <yakex@cpan.org>
2013-08-26 23:28:48 +0900

    Dispatch server and client.

---
+++ lib/App/FileShifter/Client.pm
@@ -0,0 +1,21 @@
+package App::FileShifter::Client;
+
+use strict;
+use warnings;
+
+# ABSTRACT: App::FileShifter client module
+# VERSION
+
+sub run
+{
+    my ($opts, $argv) = @_;
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
---
+++ lib/App/FileShifter/Server.pm
@@ -0,0 +1,21 @@
+package App::FileShifter::Server;
+
+use strict;
+use warnings;
+
+# ABSTRACT: App::FileShifter server module
+# VERSION
+
+sub run
+{
+    my ($opts, $argv) = @_;
+}
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
