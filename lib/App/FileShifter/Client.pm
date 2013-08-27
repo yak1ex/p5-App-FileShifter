8d3c6920c9780d60c614125a82fb0f1dbfc478e8
Yasutaka ATARASHI <yakex@cpan.org>
2013-08-27 20:00:39 +0900

    Implement channel between client and server.

--- lib/App/FileShifter/Client.pm
+++ lib/App/FileShifter/Client.pm
@@ -6,9 +6,39 @@ use warnings;
 # ABSTRACT: App::FileShifter client module
 # VERSION
 
+use AE;
+use AnyEvent::Socket;
+use AnyEvent::Handle;
+use AnyEvent::MessagePack;
+
 sub run
 {
-    my ($opts, $argv) = @_;
+    my ($class, $opts, $argv) = @_;
+
+    tcp_connect $opts->{H}, 0 + $opts->{p}, sub {
+        my ($fh) = @_ or die "Connect failed: $!";
+
+        my $handle;
+        $handle = AnyEvent::Handle->new(
+            fh => $fh,
+            on_error => sub {
+                AE::log error => $_[2];
+                $_[0]->destroy;
+            },
+            on_eof => sub {
+                $handle->destroy; # destroy handle
+                AE::log info => "Done.";
+            });
+        my $call; $call = sub {
+            my $w; $w = AE::timer 1, 0, sub {
+                $handle->push_write(msgpack => [list => [[], []]]);
+                undef $w;
+                $call->();
+            };
+        };
+        $call->();
+    };
+    AE::cv->recv;
 }
 
 1;
--- lib/App/FileShifter/Server.pm
+++ lib/App/FileShifter/Server.pm
@@ -6,9 +6,38 @@ use warnings;
 # ABSTRACT: App::FileShifter server module
 # VERSION
 
+use AE;
+use AnyEvent::Socket;
+use AnyEvent::Handle;
+use AnyEvent::MessagePack;
+
 sub run
 {
-    my ($opts, $argv) = @_;
+    my ($class, $opts, $argv) = @_;
+
+    tcp_server undef, $opts->{p}, sub {
+        my ($fh, $host, $port) = @_;
+
+        my $handle;
+        $handle = AnyEvent::Handle->new(
+            fh => $fh,
+            on_error => sub {
+                AE::log error => $_[2];
+                $_[0]->destroy;
+            },
+            on_eof => sub {
+                $handle->destroy; # destroy handle
+                AE::log info => "Done.";
+            });
+        my $handler;
+        $handler = sub {
+            my ($handle, $data) = @_;
+            print $data->[0],"\n";
+            $handle->push_read(msgpack => $handler);
+        };
+        $handle->push_read(msgpack => $handler);
+    };
+    AE::cv->recv;
 }
 
 1;
