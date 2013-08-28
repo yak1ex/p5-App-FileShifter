748bdcd2eb63b822a0005732c520ee80d6d93a21
Yasutaka ATARASHI <yakex@cpan.org>
2013-08-28 13:08:29 +0900

    Add command dispatching.

--- lib/App/FileShifter/Server.pm
+++ lib/App/FileShifter/Server.pm
@@ -11,6 +11,39 @@ use AnyEvent::Socket;
 use AnyEvent::Handle;
 use AnyEvent::MessagePack;
 
+use Digest::SHA;
+
+my %dispatch = (
+    list => \&_list,
+    get => \&_get,
+    complete => \&_complete,
+    hash => \&_hash,
+);
+
+sub _list
+{
+    my ($opts, undef, $path, $filter) = @_;
+    return [[0, 0, 0]]; # [$filename, $size, $date]...
+}
+
+sub _get
+{
+    my ($opts, undef, $file, $from, $size) = @_;
+    return [0, 0]; # $data, $sha1
+}
+
+sub _complete
+{
+    my ($opts, undef, $file, $sha1) = @_;
+    return [0]; # $status
+}
+
+sub _hash
+{
+    my ($opts, undef, $file, $from, $size) = @_;
+    return [0]; # $sha1
+}
+
 sub run
 {
     my ($class, $opts, $argv) = @_;
@@ -32,7 +65,13 @@ sub run
         my $handler;
         $handler = sub {
             my ($handle, $data) = @_;
-            print $data->[0],"\n";
+            if(exists $dispatch{$data->[0]}) {
+                $opts->{v} and print $data->[0],"\n";
+                my $ret = $dispatch{$data->[0]}($opts, @$data);
+                $handle->push_write(msgpack => $ret);
+            } else {
+                warn "Unknown command $data->[0]";
+            }
             $handle->push_read(msgpack => $handler);
         };
         $handle->push_read(msgpack => $handler);
